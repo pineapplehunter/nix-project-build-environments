fdee2963575277bd1c850d7f3ee75d69a7893405
Shogo Takata <peshogo@gmail.com>
2024-09-28 21:31:48 +0900

    add deps

--- op-tee/shell.nix
+++ op-tee/shell.nix
@@ -8,6 +8,7 @@ in
     tpkgs:
     builtins.attrValues {
       inherit (tpkgs)
+        git
         git-repo
         dtc
         ccache
@@ -22,6 +23,14 @@ in
         wget
         meson
         cmake
+        curl
+        perl
+        cpio
+        unzip
+        rsync
+        flock
+        netcat
+        xterm
         ;
       openssl = tpkgs.openssl.dev;
       glib = tpkgs.glib.dev;
@@ -37,4 +46,8 @@ in
       );
       host-cc = tpkgs.stdenv.cc;
     };
+  profile = ''
+    export NIX_SSL_CERT_FILE=${pkgs.cacert}/etc/ssl/certs/ca-bundle.crt
+    export SSL_CERT_FILE=${pkgs.cacert}/etc/ssl/certs/ca-bundle.crt
+  '';
 }).env
